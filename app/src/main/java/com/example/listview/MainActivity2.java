f6a64be27079e9792e9ec7e7baaafa414eced748
eunbii0213 <eunbii0213@naver.com>
2022-08-17 19:32:29 +0900

    feat : add circularFloatingActionMenu

--- app/src/main/java/com/example/listview/MainActivity2.java
+++ app/src/main/java/com/example/listview/MainActivity2.java
@@ -2,11 +2,16 @@ package com.example.listview;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.oguzdev.circularfloatingactionmenu.library.FloatingActionButton;
+import com.oguzdev.circularfloatingactionmenu.library.FloatingActionMenu;
+import com.oguzdev.circularfloatingactionmenu.library.SubActionButton;
 
 public class MainActivity2 extends AppCompatActivity {
     BottomNavigationView bottomNavigationView;
@@ -15,10 +20,61 @@ public class MainActivity2 extends AppCompatActivity {
     TestFragment testFragment;
     CalenderFragment calenderFragment;
     MypageFragment mypageFragment;
-    @Override protected void onCreate(Bundle savedInstanceState) {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main2);
-    bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        setContentView(R.layout.activity_main2);
+
+        //build the menu
+        ImageView icon = new ImageView(this);
+        icon.setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.button_action_dark_selector));
+
+        FloatingActionButton actionButton = new FloatingActionButton.Builder(this)
+
+                .setContentView(icon)
+
+                .build();
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+
+        //build the buttons
+        SubActionButton.Builder itemBuilder = new SubActionButton.Builder(this);
+
+        //lens button
+        ImageView lensIcon = new ImageView(this);
+        lensIcon.setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.button_action_dark_selector));
+        SubActionButton button1 = itemBuilder.setContentView(lensIcon).build();
+
+        //lensCare button
+        ImageView lensItemIcon = new ImageView(this);
+        lensItemIcon.setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.button_action_dark_selector));
+        SubActionButton button2 = itemBuilder.setContentView(lensItemIcon).build();
+
+        //Test button
+        ImageView testIcon = new ImageView(this);
+        testIcon.setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.button_action_dark_selector));
+        SubActionButton button3 = itemBuilder.setContentView(testIcon).build();
+
+        //Calendar button
+        ImageView calendarIcon = new ImageView(this);
+        calendarIcon.setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.button_action_dark_selector));
+        SubActionButton button4 = itemBuilder.setContentView(calendarIcon).build();
+
+        //memo button
+        ImageView memoItemIcon = new ImageView(this);
+        memoItemIcon.setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.button_action_dark_selector));
+        SubActionButton button5 = itemBuilder.setContentView(memoItemIcon).build();
+
+        FloatingActionMenu actionMenu = new FloatingActionMenu.Builder(this)
+
+                .addSubActionView(button1)
+                .addSubActionView(button2)
+                .addSubActionView(button3)
+                .addSubActionView(button4)
+                .addSubActionView(button5)
+                .attachTo(actionButton)
+                .build();
 
         //5개의 메뉴에 들어갈 Fragment들
         lensFragment = new LensFragment();
@@ -26,32 +82,40 @@ public class MainActivity2 extends AppCompatActivity {
         testFragment = new TestFragment();
         calenderFragment = new CalenderFragment();
         mypageFragment = new MypageFragment();
-        //메인화면 설정
-      getSupportFragmentManager().beginTransaction().replace(R.id.main_layout,lensFragment).commitAllowingStateLoss();
-      bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
 
-          @Override public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-              //각각 메뉴마다 Fragment 불러오기
-              switch (menuItem.getItemId()){
-                 case R.id.lens:{
-                     getSupportFragmentManager().beginTransaction() .replace(R.id.main_layout,lensFragment).commitAllowingStateLoss();
-                     return true;
-                 } case R.id.lensCare:{
-                     getSupportFragmentManager().beginTransaction() .replace(R.id.main_layout,lensCareFragment).commitAllowingStateLoss();
-                     return true;
-                 } case R.id.test:{
-                     getSupportFragmentManager().beginTransaction() .replace(R.id.main_layout,testFragment).commitAllowingStateLoss();
-                     return true;
-                 } case R.id.calender:{
-                      getSupportFragmentManager().beginTransaction() .replace(R.id.main_layout,calenderFragment).commitAllowingStateLoss();
-                      return true;
-                  }case R.id.myPage:{
-                      getSupportFragmentManager().beginTransaction().replace(R.id.main_layout,mypageFragment).commitAllowingStateLoss();
-                      return true;
-                  }default: return false;
-              }
-          }
-      });
+        //메인화면 설정
+        getSupportFragmentManager().beginTransaction().replace(R.id.main_layout, lensFragment).commitAllowingStateLoss();
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                //각각 메뉴마다 Fragment 불러오기
+                switch (menuItem.getItemId()) {
+                    case R.id.lens: {
+                        getSupportFragmentManager().beginTransaction().replace(R.id.main_layout, lensFragment).commitAllowingStateLoss();
+                        return true;
+                    }
+                    case R.id.lensCare: {
+                        getSupportFragmentManager().beginTransaction().replace(R.id.main_layout, lensCareFragment).commitAllowingStateLoss();
+                        return true;
+                    }
+                    case R.id.test: {
+                        getSupportFragmentManager().beginTransaction().replace(R.id.main_layout, testFragment).commitAllowingStateLoss();
+                        return true;
+                    }
+                    case R.id.calender: {
+                        getSupportFragmentManager().beginTransaction().replace(R.id.main_layout, calenderFragment).commitAllowingStateLoss();
+                        return true;
+                    }
+                    case R.id.myPage: {
+                        getSupportFragmentManager().beginTransaction().replace(R.id.main_layout, mypageFragment).commitAllowingStateLoss();
+                        return true;
+                    }
+                    default:
+                        return false;
+                }
+            }
+        });
     }
 }
 
